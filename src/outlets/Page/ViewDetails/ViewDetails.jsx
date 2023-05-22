c213f32af13680a1fdfcc7182572583f3d3dd017
SarabanTahura1 <bristiakash01796@gmail.com>
2023-05-22 22:50:11 +0600

    specifid data show on view details page

--- src/outlets/Page/ViewDetails/ViewDetails.jsx
+++ src/outlets/Page/ViewDetails/ViewDetails.jsx
@@ -3,8 +3,42 @@ import { useLoaderData } from "react-router-dom";
 
 const ViewDetails = () => {
   const makeup = useLoaderData();
-  console.log(makeup);
-  return <div></div>;
+  const {
+    sellerName,
+    email,
+    photourl,
+    toyName,
+    subcategory,
+    price,
+    quantity,
+    ratings,
+    description,
+  } = makeup;
+  return (
+    <div className="max-w-7xl mx-auto my-28 lg:flex items-center justify-center px-5 lg:px-0">
+      <div className="  w-full lg:w-5/12 flex justify-center ">
+        <img src={photourl} className="object-cover w-fit" alt="" />
+      </div>
+      <div className="w-full lg:w-7/12 px-5 space-y-3">
+        <h1 className="text-3xl font-bold text-gray-900">
+          Name : {sellerName}
+        </h1>
+        <h1 className="text-3xl font-bold text-gray-900">Email : {email}</h1>
+        <h2 className="text-xl font-bold text-gray-900">
+          Toy Name : {toyName}
+        </h2>
+        <h2 className="text-xl font-bold text-gray-900">
+          Subcategory : {subcategory}
+        </h2>
+        <h2 className="text-xl font-bold text-gray-900 ">Price : {price}</h2>
+        <h2 className="text-xl font-bold text-gray-900">Qty : {quantity}</h2>
+        <h2 className="text-xl font-bold text-gray-900 ">Ratings: {ratings}</h2>
+        <h2 className="text-xl font-bold text-gray-900">
+          Information : {description}
+        </h2>
+      </div>
+    </div>
+  );
 };
 
 export default ViewDetails;
